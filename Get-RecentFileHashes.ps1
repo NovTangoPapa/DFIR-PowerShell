5d7ef1a3a51a1bb5724b37f71804b53647a195c1
n0v <nov@novsec.net>
2019-06-27 06:07:28 -0700

    Formatting changes

--- Get-RecentFileHashes.ps1
+++ Get-RecentFileHashes.ps1
@@ -60,18 +60,19 @@ Select-Object *,"FileHash" |
 ForEach-Object{$_.URL = $_.URL -replace 'file:///', ''
 $_
 } | 
-ForEach-Object{$_.FileHash = $_.FileHash -replace '', `
-$(if($(Test-Path $_.URL) -eq $true)
-{
-    $(Get-FileHash -Path $_.URL).Hash
-}
-else
-{
-    $move = "file moved or deleted"
-    $move
-}
-    )
-$_
+ForEach-Object{
+    $_.FileHash = $_.FileHash -replace '', `
+    $(if($(Test-Path $_.URL) -eq $true)
+    {
+        $(Get-FileHash -Path $_.URL).Hash
+    }
+    else
+    {
+        $move = "file moved or deleted"
+        $move
+    }
+        )
+    $_
 }
 
 $FileHashList | Sort-Object URL,FileHash -Unique | 
